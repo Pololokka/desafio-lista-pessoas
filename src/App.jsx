0d5a71206ee4ad3450a7def36c0d10db7640f7a2
Pololokka <vitorbulbovas1@hotmail.com>
2023-04-13 17:06:32 -0300

    :sparkles: feat: feita exibição de itens

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import "./App.css";
 
+import { Pessoas } from "./Data/db";
+
 function App() {
-  return <main className="App"></main>;
+  return (
+    <>
+      <header>
+        <h1 className="titulo titulo-hover">Lista de Pessoas</h1>
+      </header>
+      <main className="App">
+        <section>
+          {Pessoas?.map((element) => (
+            <div className="person__container">
+              <p className="texto texto-hover">{element.name}</p>
+              <p className="texto texto-hover">Idade: {element.idade}</p>
+            </div>
+          ))}
+        </section>
+        <section>
+          <h2 className="subtitulo subtitulo-hover">Ordenar</h2>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
