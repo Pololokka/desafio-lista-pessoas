4bff7612d60a9af9a4a8df4c278df6e8b631ef05
Pololokka <vitorbulbovas1@hotmail.com>
2023-04-13 16:48:28 -0300

    :tada: commit inicial

---
+++ src/Data/db.js
@@ -0,0 +1,67 @@
+export const Pessoas = [
+  {
+    id: 1,
+    name: "Luiz Fernando Ribeiro",
+    idade: 25,
+  },
+  {
+    id: 2,
+    name: "Luiz Henrique Cunha",
+    idade: 28,
+  },
+  {
+    id: 3,
+    name: "Stephany da Paz",
+    idade: 19,
+  },
+  {
+    id: 4,
+    name: "Abraão Rego",
+    idade: 30,
+  },
+  {
+    id: 5,
+    name: "Yuri Porto",
+    idade: 28,
+  },
+  {
+    id: 6,
+    name: "Erick Correia",
+    idade: 30,
+  },
+  {
+    id: 7,
+    name: "Esther Mendes",
+    idade: 33,
+  },
+  {
+    id: 8,
+    name: "Lucca das Neves",
+    idade: 37,
+  },
+  {
+    id: 9,
+    name: "Tico Meria",
+    idade: 28,
+  },
+  {
+    id: 10,
+    name: "Davi Lucas Costela",
+    idade: 27,
+  },
+  {
+    id: 11,
+    name: "Thomas Silva",
+    idade: 38,
+  },
+  {
+    id: 12,
+    name: "Luna da Paz",
+    idade: 28,
+  },
+  {
+    id: 13,
+    name: "Jalim Rabei",
+    idade: 45,
+  },
+];
